36a12769f89e2dde412e066d1e6dd31d92ff450b
River.Wang <hanqing.wang@justalk.com>
2023-07-04 15:34:22 +0800

    refactor: demo 代码优化

--- app/src/main/java/com/siebeprojects/samples/keyboardheight/MainActivity.java
+++ app/src/main/java/com/siebeprojects/samples/keyboardheight/MainActivity.java
@@ -17,92 +17,66 @@
 
 package com.siebeprojects.samples.keyboardheight;
 
-import android.widget.TextView;
-import android.view.View;
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.content.res.Configuration;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.siebeprojects.keyboardheight.KeyboardHeightObserver;
 import com.siebeprojects.keyboardheight.KeyboardHeightProvider;
 
 /**
- * MainActivity that initializes the keyboardheight provider and observer. 
+ * MainActivity that initializes the keyboardheight provider and observer.
  */
 public final class MainActivity extends AppCompatActivity implements KeyboardHeightObserver {
 
-    private final static String TAG = "sample_MainActivity";
+    private final static String TAG = "MainActivity";
     private KeyboardHeightProvider keyboardHeightProvider;
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
 
         keyboardHeightProvider = new KeyboardHeightProvider(this);
-
-        // make sure to start the keyboard height provider after the onResume
-        // of this activity. This is because a popup window must be initialised
-        // and attached to the activity root view. 
-        View view = findViewById(R.id.activitylayout);
-        view.post(new Runnable() {
-                public void run() {
-                    keyboardHeightProvider.start();
-                }
-            });
+        keyboardHeightProvider.addKeyboardHeightObserver(this);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onPause() {
-        super.onPause();
-        keyboardHeightProvider.setKeyboardHeightObserver(null);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onResume() {
         super.onResume();
-        keyboardHeightProvider.setKeyboardHeightObserver(this);
+        // make sure to start the keyboard height provider after the onResume
+        // of this activity. This is because a popup window must be initialised
+        // and attached to the activity root view.
+        View view = findViewById(R.id.activitylayout);
+        view.post(() -> keyboardHeightProvider.start());
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        keyboardHeightProvider.stop();
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
-        keyboardHeightProvider.close();
+        keyboardHeightProvider.removeKeyboardHeightObserver(this);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void onKeyboardHeightChanged(int height, int orientation) {
-        String orientationLabel = orientation == Configuration.ORIENTATION_PORTRAIT ? "portrait" : "landscape";
-        Log.i(TAG, "onKeyboardHeightChanged in pixels: " + height + " " + orientationLabel);
+    public void onKeyboardHeightChanged(int height) {
+        Log.i(TAG, "onKeyboardHeightChanged in pixels: " + height);
 
-        TextView tv = (TextView)findViewById(R.id.height_text);
-        tv.setText(Integer.toString(height) + " " + orientationLabel);
+        TextView tv = (TextView) findViewById(R.id.height_text);
+        tv.setText(String.valueOf(height));
 
         // color the keyboard height view, this will remain visible when you close the keyboard
-        if (height > 0) {
-            View view = findViewById(R.id.keyboard);
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)view .getLayoutParams();
-            params.height = height;
-            view.setLayoutParams(params);
-        }
+        View view = findViewById(R.id.keyboard);
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = height;
+        view.setLayoutParams(params);
     }
 }
